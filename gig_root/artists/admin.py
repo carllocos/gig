bb59066c8ab02f90bf7b3217ebd748cb48a9aa33
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-01 13:54:54 +0100

    included Photo models into admin settings

--- gig_root/artists/admin.py
+++ gig_root/artists/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from artists.models import ArtistModel
+from artists.models import ArtistModel, Photo, ProfilePicModel, BackGroundPicModel
 # Register your models here.
 admin.site.register(ArtistModel)
+
+admin.site.register(Photo)
+admin.site.register(ProfilePicModel)
+admin.site.register(BackGroundPicModel)
