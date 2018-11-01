111b7884904be96a570f8faf700f283da0d37325
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-01 14:21:55 +0100

    Added path to an artist-profile and removed unused import

--- gig_root/artists/urls.py
+++ gig_root/artists/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 
 app_name='artists'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/<int:profile_id>/', views.view_profile, name='artist-profile'),
     path('register/', views.register_artist_view, name='register' ),
     path('register/ajax/', views.ajax_suggestions, name="suggest-ajax"),
-
 ]
