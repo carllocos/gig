1ecc63ca6d6f73d57f325487f22894056ef79317
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-02 19:13:21 +0100

    added paths to update genres, instruments, idols, stage name and bio of an artistprofile through Ajax Request

--- gig_root/artists/urls.py
+++ gig_root/artists/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('profile/<int:profile_id>/', views.view_profile, name='artist-profile'),
     path('register/', views.register_artist_view, name='register' ),
     path('register/ajax/', views.ajax_suggestions, name="suggest-ajax"),
+    path('profile/update_genre_idol_instrument/', views.update_genre_idol_instrument, name="update-genres-idols-insts"),
+    path('profile/update_stage_name/', views.update_stage_name, name="update-stage-name"),
 ]
