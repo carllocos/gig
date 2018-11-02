57396cee352beb75704beee0c37f726a3a7e792f
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-02 19:16:26 +0100

    Bug fix: included path to update-biography through Ajax requests

--- gig_root/artists/urls.py
+++ gig_root/artists/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/ajax/', views.ajax_suggestions, name="suggest-ajax"),
     path('profile/update_genre_idol_instrument/', views.update_genre_idol_instrument, name="update-genres-idols-insts"),
     path('profile/update_stage_name/', views.update_stage_name, name="update-stage-name"),
+    path('profile/update_biography/', views.update_biography, name="update-biography"),
 ]
