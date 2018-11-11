fb2697834a8d0b663e1d855d8d16b88dee0acb70
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-11 15:17:49 +0100

    added model Participant and convenience methods to manage comments, votes and participants

--- gig_root/events/models.py
+++ gig_root/events/models.py
@@ -4,10 +4,19 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+
 from users.sharedModels import CommentAbstract, PictureAbstract, VoteAbstract
+from users.models import User
 from musicians.models import Band
 
 DEFAULT_EVENT_PIC = path= os.path.join(settings.STATICFILES_DIRS [0], "pics/event_default.jpg")
+
+def str_to_int(s , default=False):
+    try:
+        return int(float(s))
+    except ValueError:
+        return default
+
 
 class EventPicture(PictureAbstract):
     def __init__(self, *args, **kwargs):
@@ -33,18 +42,231 @@ class Event(models.Model):
     def __str__(self):
         return f'event {self.name} for {self.band.name}'
 
+
+    def add_comment(self, msg, commentator):
+        """
+        add_comment method creates a EventComment instance with `msg` as the comment
+        and `commentator` as a User instance that commented.
+
+        Raises a ValueError if `msg` is an empty string or `commentator` is not a User
+        instance
+        """
+        if msg == "":
+            raise ValueError("`msg` cannot be an empty string")
+        if not isinstance(commentator, User):
+            raise ValueError("`commentator` has to be a `User` type")
+
+        return self.eventcomment_set.create(comment=msg, commentator=commentator)
+
+    def get_comment(self, comment_pk, default=False):
+        """
+        get_comment returns a comment with primary key equal to `pk`.
+        If the comment don't exists `default` is returned
+        """
+        try:
+            return self.eventcomment_set.get(pk=str_to_int(comment_pk))
+        except:
+            return default
+
+    def get_comments(self):
+        """
+        returns a queryset of all comments ordered.
+        """
+        return self.eventcomment_set.all()
+
+    def delete_comment(self, pk, default=False):
+        """
+        delete_comment removes band comment with primary key equal to `pk`.
+        Returns True if operation went well. Otherwise `default` is returned
+        """
+        try:
+            self.eventcomment_set.get(pk=pk).delete()
+            return True
+        except:
+            return default
+
+    def upvote(self, comment_pk, user):
+        """
+        `upvote` upvotes comment with primary key `comment_pk` where `user` is the voter.
+        Returns the newly created vote instance or False.
+        """
+        c=self.get_comment(comment_pk)
+        if not c:
+            raise ValueError(f'No comment associated with primary key `{comment_pk}`')
+
+        return c.upvote(user)
+
+    def downvote(self, comment_pk, user):
+        """
+        `downvote` downvotes comment with primary key `comment_pk` where `user` is the voter.
+        Returns the newly created vote instance or False.
+        """
+        c=self.get_comment(comment_pk)
+        if not c:
+            raise ValueError(f'No comment associated with primary key `{comment_pk}`')
+
+        return c.downvote(user)
+
+
+    def get_participant(self, user):
+        if self.is_participant(user):
+            return self.participant_set.get(participant=user)
+        return False
+
+    def get_participants(self):
+        return self.participant_set.all()
+
+    def add_participant(self, user):
+        if not self.is_participant(user):
+            return self.participant_set.create(participant=user)
+        return False
+
+    def remove_participant(self, user):
+        p=self.get_participant(user)
+        if p:
+            p.delete()
+            return True
+        return False
+
+    def is_participant(self, user):
+        return self.participant_set.filter(participant=user).exists()
+
+    @property
+    def amount_participants(self):
+        return self.get_participants().count()
+        
 @receiver(pre_delete, sender=Event)
 def delete_pic(sender, instance, **kwargs):
     """
     This function will be called before an Event instance is deleted. To remove the associated pic of the event
     """
-    print("Im back here")
     EventPicture.delete_pics([instance.picture])
 
 
 
 class EventComment(CommentAbstract):
-    pass
+    """
+    Represents a comment associated to an event.
+    """
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+
+    @property
+    def upvotes(self):
+        """Get the amount of upvotes for this comment."""
+        return  self.amount_upVotes()
+    @property
+    def downvotes(self):
+        """Get the amount of downvotes for this comment."""
+        return  self.amount_downVotes()
+
+    def __str__(self):
+        return f'Event Comment' + super(EventComment, self).__str__()
+
+    def get_vote(self, voter, default=False):
+        """
+        get_vote returns a vote associated to `voter`. `voter` can be a string or int primary_key
+        or a User instance. If no vote exists for `voter`, `default` is returned
+        """
+        if isinstance(voter, int) or isinstance(voter, str):
+            try:
+                return self.commentvote_set.get(pk=str_to_int(voter))
+            except:
+                return default
+        elif isinstance(voter, User):
+            try:
+                return self.commentvote_set.get(voter=voter)
+            except:
+                return default
+        else:
+            raise TypeError("Wrong type for `voter`. Voter can only be a int, str representing a primary key. Or a User instance")
+
+    def get_votes(self, only_up=None):
+        """
+        get_votes returns by default all votes for this comment.
+        `only_up` set to true returns only upvotes. Set to false only downvotes
+        """
+        if only_up is None:
+            return self.commentvote_set.all()
+        if only_up:
+            return self.commentvote_set.filter(is_upvote=True)
+        else:
+            return self.commentvote_set.filter(is_upvote=False)
+
+    def amount_upVotes(self):
+        """
+        Returns the amount of upvotes for this comment
+        """
+        return self.commentvote_set.filter(is_upvote=True).count()
+
+    def amount_downVotes(self):
+        """
+        Returns the amount of downvotes for this comment
+        """
+        return self.commentvote_set.filter(is_upvote=False).count()
+
+    def already_voted(self, user):
+        """
+        already_voted checks whether `user` already voted this comment
+        """
+        return self.commentvote_set.filter(voter=user).exists()
+
+
+    def __vote(self, user, is_upvote):
+        if self.already_voted(user):
+            return False
+        else:
+            return self.commentvote_set.create(voter=user, is_upvote=is_upvote)
+
+    def upvote(self, user):
+        """
+        upvote will create a CommentVote instance with `is_upvote` set to True
+        and `voter` set to `user`.
+
+        This method calls `already_voted` first to ensure that the `user` didn't already
+        vote. If the `user` already voted False is returned otherwise the new instance is returned.
+        """
+        return self.__vote(user, is_upvote=True)
+
+    def downvote(self, user):
+        """
+        downvote will create a CommentVote instance with `is_upvote` set to False
+        and `voter` set to `user`.
+
+        This method calls `already_voted` first to ensure that the `user` didn't already
+        vote. If the `user` already voted False is returned otherwise the new instance is returned.
+        """
+        return self.__vote(user, is_upvote=False)
+
+    def inverse_vote(self, user):
+        """
+        inverse_vote will inverse the current vote of the `user`.
+        E.g. `is_upvote` equal to True becomes False
+
+        This method assumes that `already_voted` was called to ensure that the vote exists
+        for the voter `user`.
+        """
+        v=self.commentvote_set.get(voter=user)
+        v.is_upvote = not v.is_upvote
+        v.save()
+
+
 
 class CommentVote(VoteAbstract):
-    pass
+    """
+    Vote meant to represent the upvotes or downvotes towards one Comment of an Event.
+    """
+    comment=models.ForeignKey(EventComment, on_delete=models.CASCADE)
+    def __str__(self):
+        v= 'upvote' if self.is_upvote else 'downvote'
+        return f'{v} for {self.comment}'
+
+class Participant(models.Model):
+    """
+    Model to represent the amount of Participants for one Event instance.
+    """
+    event=models.ForeignKey(Event, on_delete=models.CASCADE)
+    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.participant.first_name} {self.participant.last_name} participates in {self.event.name}'
