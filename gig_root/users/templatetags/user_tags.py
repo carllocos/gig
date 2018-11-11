127e5fc976b7152107557391616b7b1dd2485eb8
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-11 13:15:25 +0100

    created tag to add classes to html elements

--- gig_root/users/templatetags/user_tags.py
+++ gig_root/users/templatetags/user_tags.py
@@ -15,3 +15,18 @@ def remove(match=None, arg=None):
 @register.filter(name='remove_meta')
 def remove_meta(arg=None):
     return remove(match='__all__',arg=arg)
+
+
+@register.filter(name='add_attr')
+def add_attr(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' not in d:
+            attrs['class'] = d
+        else:
+            key, val = d.split(':')
+            attrs[key] = val
+
+    return field.as_widget(attrs=attrs)
