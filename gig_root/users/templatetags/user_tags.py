263df995e3aa654debbd7cb39a02ba20c71f0ee1
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-12 13:02:46 +0100

    added filters for fancy date display and correct time display

--- gig_root/users/templatetags/user_tags.py
+++ gig_root/users/templatetags/user_tags.py
@@ -1,3 +1,4 @@
+import datetime
 
 from django import template
 
@@ -30,3 +31,25 @@ def add_attr(field, css):
             attrs[key] = val
 
     return field.as_widget(attrs=attrs)
+
+
+@register.filter(name='correct_time')
+def correct_time(hour, minute):
+    str_h= '0' + str(hour) if hour < 10 else str(hour)
+    str_min= '0' + str(minute) if minute < 10 else str(minute)
+
+    return str_h + ':' + str_min
+
+@register.filter(name='fancy_date')
+def fancy_date(event_date_time):
+    today=datetime.date.today()
+    event_date=event_date_time.date()
+
+    if today == event_date:
+        return "Today at " + str(event_date_time.hour) + ":" + str(event_date_time.minute)
+    elif today  + datetime.timedelta(days=1) == event_date:
+        return "Tomorrow at " + str(event_date_time.hour) + ":" + str(event_date_time.minute)
+    elif today  > event_date:
+        return "Already played"
+    else:
+        return event_date_time
