b93bdf30e2fe21385992343cab6c181067b3633e
carllocos <carlos.javier.montero.castillo@vub.be>
2018-11-12 13:07:53 +0100

    added path to edit event view and ajax requests

--- gig_root/events/urls.py
+++ gig_root/events/urls.py
@@ -9,5 +9,12 @@ app_name = 'events'
 urlpatterns = [
 path("create/", views.create_event, name="create"),
 path("profile/<int:event_id>", views.event_profile, name="profile"),
+path("edit/<int:event_id>", views.event_edit, name="edit"),
+path("edit/update_name", views.update_name, name="update-name"),
+path("edit/update_description", views.update_description, name="update-description"),
+path("edit/update_date", views.update_date, name="update-date"),
+path("edit/update_time", views.update_time, name="update-time"),
+path("edit/update_band", views.update_band, name="update-band"),
+path("edit/update_picture", views.update_picture, name="update-picture"),
 
 ]
