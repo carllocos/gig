8f2cc84877c81b135216190e875c95afc6542d42
carllocos <carlos.javier.montero.castillo@vub.be>
2018-12-15 16:22:29 +0100

    added documentation and deleted debug statements

--- gig_root/static/events/js/map_util.js
+++ gig_root/static/events/js/map_util.js
@@ -1,27 +1,38 @@
+/*
+map_util.js module is meant to abstract over the Mapbox Geocoder API.
+*/
+
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW50ZW1vb28iLCJhIjoiY2pwOTB5ZDBjMDJidzN1cWoxOWZncGdhdSJ9.dJg1hseRjqfD9aHXjXfNtA';
 var geocoder=null;
 
 function createGeocoder(){
+  /*
+  creates a geocoder object.
+  */
   geocoder=L.mapbox.geocoder("mapbox.places");
   return geocoder;
 }
 
 
 
-
 function queryAddress(id_selector, callback){
+  /*
+  `queryAddress` performs a query to mapbox with address provided by inputfield with id `id_selector`.
+  the `callback` function is executed with the response.
+  */
   var address = $("#"+id_selector).val();
   if (address != ""){
     geocoder.query({query: address}, callback);
-  }
-  else {
-    console.log("Nos search performed the query is empty");
   }
 }
 
 
 function queryAddressSync(id_selector){
+  /*
+  `queryAddressSync` performs a synchronous query request to mapbox with address provided by inputfield with id `id_selector`.
+  tThe most relevant result is returned from the query.
+  */
   var address=$("#"+id_selector).val();
   var q=encodeURIComponent(address);
   var remote_url= "http://a.tiles.mapbox.com/geocoding/v5/mapbox.places/"+q +".json?access_token="+L.mapbox.accessToken;
