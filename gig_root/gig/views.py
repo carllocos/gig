9ffae91e0677971101eaa82debec464452941bf7
carllocos <carlos.javier.montero.castillo@vub.be>
2018-12-22 00:16:40 +0100

    refactoring; delete unnecessary code

--- gig_root/gig/views.py
+++ gig_root/gig/views.py
@@ -8,27 +8,18 @@ from . import search as dbSearch
 
 def home(request):
     """
-    The home page of gigForMusicians.
-    The home page focus on displaying several kinds of upcoming events.
-    Depending whether the user is logged in or not.
+    The home page of gigForMusicians, focus on displaying upcoming events.
     """
 
     user=request.user
     has_artistProfile=user.has_artistProfile() if user.is_authenticated else False
     upc_evs=dbSearch.get_upcoming_events()
-    foll_b_evs= dbSearch.get_follow_bands_events(user) if user.is_authenticated else False
-    part_evs= dbSearch.get_participate_events(user) if user.is_authenticated else False
-    might_like_evs= dbSearch.get_might_like_events(user) if user.is_authenticated else False
-
     random_bands= dbSearch.get_randomBands(amount=1)
 
     context= {
         'user': user,
         'has_artistProfile': has_artistProfile,
         'upcoming_events': upc_evs,
-        'follow_band_events': foll_b_evs,
-        'participate_events': part_evs,
-        'might_like_events': might_like_evs,
         'random_bands': random_bands,
         'http_protocol': getHTTP_Protocol()
     }
